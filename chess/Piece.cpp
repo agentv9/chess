02965e215357eca18746b3f924ed8d688c79dadb
agentv9 <agentawesome323@gmail.com>
2022-12-23 15:53:58 -0600

    stuff and things

---
+++ chess/Piece.cpp
@@ -0,0 +1,14 @@
+
+class Piece {
+public: 
+	int type; // 1: pawn, 2: bishop, 3: knight, 4: rook, 5: queen, 6: king
+	
+	int GetMove(char chessboard, int posx, int posy);
+};
+
+int Piece::GetMove(char chessboard, int posx, int posy) {
+
+
+
+	return 1;
+}
---
+++ chess/chess.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+	char chessboard[8][8];
+	int x;
+	int y;
+
+
+	for ( x = 0; x < 8; x++) {
+		for ( y = 0; y < 8; y++) {
+			if ((y + x) % 2) {
+				chessboard[x][y] = 'W';
+			}
+			else {
+				chessboard[x][y] = 'B';
+			}
+		}
+	}
+
+	puts("Chessboard example");
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			printf("%c\t", chessboard[x][y]);
+		putchar('\n');
+	}
+	return(0);
+
+}
+
